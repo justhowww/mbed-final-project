a5d1b16ef6d6b5446fa38a027595f4035bd4328b
justhowww <73697339+JustinHuang1111@users.noreply.github.com>
2023-12-08 12:10:35 +0800

    mediapipe streaming test finished

---
+++ main.py
@@ -0,0 +1,16 @@
+import mediapipe as mp
+
+# -- detect a "starting" hand gesture
+
+
+# -- start gaming loop
+
+while 1:
+    pass
+    # -- send starting message to stm32
+
+    # -- receive response from stm32
+
+    # -- simulate the game using the response
+
+    # -- loop again once a "getting the ball" gesture is detected
