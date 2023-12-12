04301a4fc93836db255522fae8f7601d950ef709
justhowww <73697339+JustinHuang1111@users.noreply.github.com>
2023-12-12 22:19:36 +0800

    try to move to page

--- air-basket/app/components/cam.tsx
+++ air-basket/app/components/cam.tsx
@@ -14,21 +14,21 @@ export default function Cam() {
   const detectorRef = useRef<any>();
   const videoRef = useRef<HTMLVideoElement>();
   // const [ctx, setCtx] = useState<CanvasRenderingContext2D>();
-  async function setupDetector() {
-    const model = SupportedModels.MediaPipeHands;
-    const detector = await createDetector(model, {
-      runtime: "mediapipe",
-      maxHands: 2,
-      solutionPath: "https://cdn.jsdelivr.net/npm/@mediapipe/hands",
-    });
+  // async function setupDetector() {
+  //   const model = SupportedModels.MediaPipeHands;
+  //   const detector = await createDetector(model, {
+  //     runtime: "mediapipe",
+  //     maxHands: 2,
+  //     solutionPath: "https://cdn.jsdelivr.net/npm/@mediapipe/hands",
+  //   });
 
-    return detector;
-  }
+  //   return detector;
+  // }
   useEffect(() => {
     async function initialize() {
       videoRef.current = (await setupVideo()) as HTMLVideoElement;
       const ctx = await setupCanvas(videoRef.current);
-      detectorRef.current = await setupDetector();
+      // detectorRef.current = await setupDetector();
       // setCtx(ctx || undefined);
     }
     initialize();
