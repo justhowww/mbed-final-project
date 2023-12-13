57e4d87d290bd1158cc84b987c95a2a553a9bdbd
justhowww <73697339+JustinHuang1111@users.noreply.github.com>
2023-12-13 14:49:23 +0800

    flask test

--- airbasket/next.config.js
+++ airbasket/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+};
+module.exports = {
+  ...nextConfig,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:5328/:path*", // Proxy to Backend
+      },
+    ];
+  },
+};
